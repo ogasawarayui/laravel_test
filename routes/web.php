30343eee410a9f799a8795ab865a842215fc194f
ogasawarayui <ogasawara.yui@outlook.com>
2024-08-01 22:06:37 +0900

    app

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TodoListController;
+use App\Http\Controllers\TaskController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,7 +13,11 @@ use App\Http\Controllers\TodoListController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/list', [TodoListController::class, 'index']);
+Route::get('/', [TodoListController::class, 'index']);//一覧表示
+Route::post('/create',[TaskController::class,'create']);//タスク追加
+Route::post('/edit',[TaskController::class,'edit']);//タスク更新
+Route::post('/delete',[TaskController::class,'delete']);//タスク削除
+Route::resource('tasks', TaskController::class);
 Route::get('/', function () {
     return view('welcome');
 });

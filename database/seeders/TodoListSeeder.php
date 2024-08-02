f315c18bfc26d2070b0193f7a9271a069afa029e
ogasawarayui <ogasawara.yui@outlook.com>
2024-08-02 14:49:46 +0900

    app

--- database/seeders/TodoListSeeder.php
+++ database/seeders/TodoListSeeder.php
@@ -8,26 +8,26 @@ use Illuminate\Support\Facades\DB;
 
 class TodoListSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        DB::table('todo_lists')->insert(
-            [
-              [
-                'name' => 'テスト1',
-                'created_at' => now(),
-                'updated_at' => now(),
-              ],
-              [
-                'name' => 'テスト2',
-                'created_at' => now(),
-                'updated_at' => now(),
-              ],
-            ]
-          );
-    }
+  /**
+   * Run the database seeds.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    DB::table('todo_lists')->insert(
+      [
+        [
+          'name' => 'テスト1',
+          'created_at' => now(),
+          'updated_at' => now(),
+        ],
+        [
+          'name' => 'テスト2',
+          'created_at' => now(),
+          'updated_at' => now(),
+        ],
+      ]
+    );
+  }
 }

9327786136f1acc3632ac7ac6f2664b7a21d2f11
ogasawarayui <ogasawara.yui@outlook.com>
2024-08-02 13:17:47 +0900

    app

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -15,7 +15,8 @@ class TaskController extends Controller
      */
     public function index()
     {
-        $tasks = Task::all();
+        $tasks = Task::where('status', false)->get();
+
         return view('tasks.index', compact('tasks'));
     }
 
@@ -39,25 +40,25 @@ class TaskController extends Controller
     {
         $rules = [
             'task_name' => 'required|max:100',
-          ];
-         
-          $messages = ['required' => '必須項目です', 'max' => '100文字以下にしてください。'];
-         
-          Validator::make($request->all(), $rules, $messages)->validate();
-         
-         
-          
-          //モデルをインスタンス化
-          $task = new Task;
-         
-          //モデル->カラム名 = 値 で、データを割り当てる
-          $task->name = $request->input('task_name');
-         
-          //データベースに保存
-          $task->save();
-         
-          //リダイレクト
-          return redirect('/tasks');
+        ];
+
+        $messages = ['required' => '必須項目です', 'max' => '100文字以下にしてください。'];
+
+        Validator::make($request->all(), $rules, $messages)->validate();
+
+
+
+        //モデルをインスタンス化
+        $task = new Task;
+
+        //モデル->カラム名 = 値 で、データを割り当てる
+        $task->name = $request->input('task_name');
+
+        //データベースに保存
+        $task->save();
+
+        //リダイレクト
+        return redirect('/tasks');
     }
 
     /**
@@ -92,7 +93,41 @@ class TaskController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        //「編集する」ボタンをおしたとき
+        if ($request->status === null) {
+            $rules = [
+                'task_name' => 'required|max:100',
+            ];
+
+            $messages = ['required' => '必須項目です', 'max' => '100文字以下にしてください。'];
+
+            Validator::make($request->all(), $rules, $messages)->validate();
+
+
+            //該当のタスクを検索
+            $task = Task::find($id);
+
+            //モデル->カラム名 = 値 で、データを割り当てる
+            $task->name = $request->input('task_name');
+
+            //データベースに保存
+            $task->save();
+        } else {
+            //「完了」ボタンを押したとき
+
+            //該当のタスクを検索
+            $task = Task::find($id);
+
+            //モデル->カラム名 = 値 で、データを割り当てる
+            $task->status = true; //true:完了、false:未完了
+
+            //データベースに保存
+            $task->save();
+        }
+
+
+        //リダイレクト
+        return redirect('/tasks');
     }
 
     /**
@@ -103,6 +138,8 @@ class TaskController extends Controller
      */
     public function destroy($id)
     {
-        //
+        Task::find($id)->delete();
+
+        return redirect('/tasks');
     }
 }
